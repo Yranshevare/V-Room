09cdccc07ba468fcdcaf7529fd3e1c698c293cbe
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-10-05 16:27:33 +0530

    liked frontend with backend

--- Frontend/src/constant.ts
+++ Frontend/src/constant.ts
@@ -1,5 +1,5 @@
 // const websocketServerUrl = "http://localhost:4000";
-const websocketServerUrl = "https://v-room-git-main-yranshevares-projects.vercel.app/";
+const websocketServerUrl = "https://v-room.onrender.com";
 const appServer = "http://localhost:3000";
 
 export { websocketServerUrl, appServer };
