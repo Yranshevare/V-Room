740df58f7fdad941d9e6a54b9585cf3bffc39c96
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-10-05 16:55:24 +0530

    added frontend url for production

--- Frontend/src/constant.ts
+++ Frontend/src/constant.ts
@@ -1,5 +1,6 @@
 // const websocketServerUrl = "http://localhost:4000";
 const websocketServerUrl = "https://v-room.onrender.com";
-const appServer = "http://localhost:3000";
+// const appServer = "http://localhost:3000";
+const appServer = "https://v-room.vercel.app";
 
 export { websocketServerUrl, appServer };
